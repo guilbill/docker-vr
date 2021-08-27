77cf48dbd32bba9a1a97966dbb87607df146d93f
Guillaume BILLEY <guilbill@gmail.com>
2021-08-27 15:01:46 +0200

    Add Suspense

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, { Suspense } from 'react';
 import './styles.css';
 import Scene from './scene/Scene';
 
 function App() {
-    return <Scene />;
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Scene />
+        </Suspense>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
