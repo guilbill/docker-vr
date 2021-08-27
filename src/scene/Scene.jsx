a7479c6bc1d2c10759fabd935822dbdc58c237d2
Guillaume BILLEY <guilbill@gmail.com>
2021-08-27 11:37:10 +0200

    Add texture

--- src/scene/Scene.jsx
+++ src/scene/Scene.jsx
@@ -1,17 +1,20 @@
-import { Box } from '@react-three/drei';
+import { Box, useMatcapTexture } from '@react-three/drei';
 import { DefaultXRControllers, Hands, VRCanvas } from '@react-three/xr';
 import React from 'react';
 
-const Scene = () => (
-    <VRCanvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <DefaultXRControllers />
-        <Box position={[-1, 0, -4]} castShadow>
-            <meshMatcapMaterial attach="material" />
-        </Box>
-        <Hands />
-    </VRCanvas>
-);
+const Scene = () => {
+    const [texture] = useMatcapTexture('045C5C_0DBDBD_049393_04A4A4');
+    return (
+        <VRCanvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <DefaultXRControllers />
+            <Box position={[-1, 0, -4]} castShadow>
+                <meshMatcapMaterial attach="material" matcap={texture} />
+            </Box>
+            <Hands />
+        </VRCanvas>
+    );
+};
 
 export default Scene;
