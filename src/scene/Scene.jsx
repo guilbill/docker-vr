93507f6dbb8122ba1e6329a1ddb79423e31d0132
Guillaume BILLEY <guilbill@gmail.com>
2021-08-27 12:00:33 +0200

    Texturize ground

--- src/scene/Scene.jsx
+++ src/scene/Scene.jsx
@@ -4,10 +4,13 @@ import React from 'react';
 
 const Scene = () => {
     const [texture] = useMatcapTexture('045C5C_0DBDBD_049393_04A4A4');
+    const [groundTexture] = useMatcapTexture('050505_747474_4C4C4C_333333');
     return (
         <VRCanvas>
             <Sky />
-            <Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]} />
+            <Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]}>
+                <meshMatcapMaterial attach="material" matcap={groundTexture} />
+            </Plane>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
             <DefaultXRControllers />
