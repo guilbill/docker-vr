481c79bfe12fa3a1bafe670cb343c773c8b2861e
Guillaume BILLEY <guilbill@gmail.com>
2021-08-27 15:01:37 +0200

    Better texturize ground and Box

--- src/scene/Scene.jsx
+++ src/scene/Scene.jsx
@@ -1,28 +1,52 @@
-import { Box, Plane, Sky, useMatcapTexture } from '@react-three/drei';
+import {
+    Box,
+    Plane,
+    Sky,
+    useMatcapTexture,
+    useNormalTexture,
+} from '@react-three/drei';
 import { DefaultXRControllers, Hands, VRCanvas } from '@react-three/xr';
 import React from 'react';
 
 const Scene = () => {
-    const [texture] = useMatcapTexture('045C5C_0DBDBD_049393_04A4A4');
-    const [groundTexture] = useMatcapTexture('050505_747474_4C4C4C_333333');
+    const [texture] = useNormalTexture(52, {
+        offset: [0, 0],
+        repeat: [0.5, 0.5],
+        anisotropy: 1,
+    });
+    const [groundTexture] = useNormalTexture(55, {
+        offset: [0, 0],
+        repeat: [100, 100],
+        anisotropy: 1,
+    });
     return (
         <VRCanvas>
             <Sky />
             <Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]}>
-                <meshMatcapMaterial attach="material" matcap={groundTexture} />
+                <meshStandardMaterial
+                    normalMap={groundTexture}
+                    color="#848282"
+                />
             </Plane>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
             <DefaultXRControllers />
             <Box position={[-2, 1.1, -4]} castShadow>
-                <meshMatcapMaterial attach="material" matcap={texture} />
+                <meshStandardMaterial normalMap={texture} color="#313241" />
             </Box>
             <Box position={[0, 1.1, -4]} castShadow>
-                <meshMatcapMaterial attach="material" matcap={texture} />
+                <meshStandardMaterial normalMap={texture} color="#313241" />
             </Box>
             <Box position={[2, 1.1, -4]} castShadow>
-                <meshMatcapMaterial attach="material" matcap={texture} />
+                <meshStandardMaterial normalMap={texture} color="#313241" />
             </Box>
+            <spotLight
+                position={[1, 8, 1]}
+                angle={0.3}
+                penumbra={1}
+                intensity={1}
+                castShadow
+            />
             <Hands />
         </VRCanvas>
     );
